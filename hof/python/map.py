42219510069a7e3fd5b669fce82486a2cc6dd227
Gil Sousa <tiagosousafl@gmail.com>
2024-09-22 13:03:59 +0300

    Add python map function examples

---
+++ hof/python/map.py
@@ -0,0 +1,11 @@
+def map1(fn, list):
+    if list == []:
+        return []
+
+    return [fn(list[0])] + map1(fn, list[1:])
+
+def map2(fn, list):
+    return [fn(x) for x in list]
+
+print(map1(lambda x: x + 2, [1, 2, 3]))
+print(map2(lambda x: x + 2, [1, 2, 3]))
