873e766c43b73b12042e97fe36ef5f3194e147ca
Gil Sousa <tiagosousafl@gmail.com>
2024-09-22 13:05:12 +0300

    Add code comments

--- hof/python/map.py
+++ hof/python/map.py
@@ -1,9 +1,11 @@
+# Simple map function implementation
 def map1(fn, list):
     if list == []:
         return []
 
     return [fn(list[0])] + map1(fn, list[1:])
 
+# Map function using list comprehension
 def map2(fn, list):
     return [fn(x) for x in list]
 
